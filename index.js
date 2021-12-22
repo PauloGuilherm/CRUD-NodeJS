cf8bb9134a1bd7d878bdcda938b3bc45ec40c3d4
PauloGuilherm <paulo.guilherme.arruda1@gmail.com>
2021-12-22 11:41:48 -0300

    validacoes e refatoracao das rotas

--- index.js
+++ index.js
@@ -12,24 +12,58 @@ app.listen(port, () => {
 })
 
 app.get('/', async(req, res) => {
-    const select = await User.findAll()
+    const select = await User.findAll();
+    res.json(select)
 })
 
 app.get('/:id', async(req, res) => {
     const selectId = await User.findByPk(req.params.id)
+    res.json(selectId)
 })
 
 app.post('/cadastro', async(req, res) => {
-    User.create(req.body)
+    await User.create(req.body)
+        .then(() => {
+            return res.json({ message: 'Usuário cadastrado com sucesso' })
+        }).catch(() => {
+            return res.status(400).json({ message: 'Erro ao cadastrar usuário' })
+        })
+
 })
 
-app.put('/editar/:id', async(req, res) => {
-    const editID = await User.findByPk(req.params.id)
-    editID.name = req.body.name
+app.patch('/editar/:id', async(req, res) => {
+    let editID = await User.findByPk(req.params.id)
+
+    if (req.body.name != undefined) {
+        editID.name = req.body.name
+    }
+    if (req.body.email != undefined) {
+        editID.email = req.body.email
+    }
+    if (req.body.telefone != undefined) {
+        editID.telefone = req.body.telefone
+    }
+    if (req.body.profissao != undefined) {
+        editID.profissao = req.body.profissao
+    }
+
     editID.save()
+        .then(() => {
+            return res.json({ message: 'Usuário editado com sucesso' })
+        }).catch(() => {
+            return res.status(400).json({ message: 'Erro ao editar usuário' })
+        })
 })
 
 app.delete('/delete/:id', async(req, res) => {
-    const deletID = await User.findByPk(req.params.id)
+    let deletID = await User.findByPk(req.params.id)
+    if (deletID == null) {
+        return res.json({ message: 'ID inválido, usuário inexistente' })
+    }
     deletID.destroy()
+        .then(() => {
+            return res.json({ message: 'Usuário deletado com sucesso' })
+        }).catch(() => {
+            return res.status(400).json({ message: 'Erro ao deletar usuário' })
+        })
 })
